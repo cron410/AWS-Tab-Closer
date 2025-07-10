719667ce4cb66740061a6a9fdbd3075151e5ccd2
cron410 <3082899+cron410@users.noreply.github.com>
2025-07-10 11:53:21 -0400

    rewrite content.js with hostname list and body list

--- src/content.js
+++ src/content.js
@@ -1,3 +1,15 @@
-if (window.location.host === "127.0.0.1:35001" && document.body.innerText.includes("Authentication details received, processing details. You may close this window at any time.")) {
+const validHosts = [
+  "127.0.0.1:35001", 
+  "127.0.0.1:58377"
+];
+const validMessages = [
+  "Authentication details received, processing details. You may close this window at any time.",
+  "AWS CLI has been given requested permissions"
+];
+
+if (
+  validHosts.includes(window.location.host) &&
+  validMessages.some(msg => document.body.innerText.includes(msg))
+) {
   browser.runtime.sendMessage("close-tab");
 }
